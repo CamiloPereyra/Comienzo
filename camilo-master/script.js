c51696c58c1ff7bd103e488b77bc419085662d4c
Camilo Pereyra <63877199+CamiloPereyra@users.noreply.github.com>
2020-05-04 14:00:48 -0300

    Add files via upload
    TAREA 2, TAREA 3

---
+++ camilo-master/script.js
@@ -0,0 +1,11 @@
+let nombre;
+let apellido;
+let edad;
+alert("Bienvenido");
+nombre=prompt("Ingrese su Nombre:");
+apellido=prompt("Ingrese su Apellido:");
+edad=prompt("Ingrese su Edad:");
+console.log("Nombre completo:",nombre,apellido+".");
+console.log("Edad:",edad+".");
+
+console.log("Usted vivió aproximadamente:",edad*365+".");
